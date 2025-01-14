16146579eb4fc3c68ec22a5d185a877f564b335a
Axemark38 <mark.p0rtn0y.888@protonmail.com>
2025-01-14 14:09:24 +0200

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -1,7 +1,7 @@
 const fetchButton = document.getElementById("fetch");
 fetchButton.addEventListener("click", async () => {
   try {
-    const response = await fetch("https://videogamesplus.ca");
+    const response = await fetch("https://videogamesplus.ca", { headers: {"Origin": "https://axemark38.github.io"} });
     console.log(response.status);
     
   } catch (error){console.log(error)}
