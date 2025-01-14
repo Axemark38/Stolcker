825cc32a60186f4042303005257286eeb11a0012
Axemark38 <mark.p0rtn0y.888@protonmail.com>
2025-01-14 14:23:33 +0200

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -4,7 +4,7 @@ fetchButton.addEventListener("click", async () => {
     const response = await fetch("https://videogamesplus.ca", {
       headers: {
         "Origin": "https://axemark38.github.io",
-        "Access-Control-Allow-Origin": *
+        "Access-Control-Allow-Origin": "*"
       }});
     console.log(response.status);
     
