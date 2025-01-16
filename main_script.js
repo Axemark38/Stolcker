8d032c5de7b8ade704b27373086cc49e7353a1de
Axemark38 <mark.p0rtn0y.888@protonmail.com>
2025-01-16 23:47:29 +0200

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -2,7 +2,7 @@ const fetchButton = document.getElementById("fetch");
 fetchButton.addEventListener("click", async () => {
     const response = await fetch("https://videogamesplus.ca", {
       headers: {
-        "Content-Security-Policy": "connect-src https:";
+        "Content-Security-Policy": "connect-src https:"
       }});
     console.log(response.status);
 })
